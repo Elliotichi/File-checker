54c32d7da26adc704bd22f03aa3a2c062c5b14f4
elliotichi <e.priestley@rgu.ac.uk>
2025-02-06 18:41:28 +0000

    comments added to program.cs

--- Program.cs
+++ Program.cs
@@ -7,16 +7,17 @@ public class Program
 
     static void Main(string[] args)
     {
+        // Checks if there is a commandline argument.
         if (args.Length == 0)
         {
             Console.WriteLine("Error: program needs a filepath to run");
             loop = false;
         }
 
-      
+        // checker object
         Checker checker = new(args);
        
-
+        // option menu loop, ask user for an option.
         while (loop)
         {
             Console.WriteLine("=======================");
@@ -25,16 +26,19 @@ public class Program
             Console.WriteLine("[2] Toggle Case Sensitivity (Default on)");
             Console.WriteLine("[3] Exit Program");
             Console.WriteLine("=======================");
+
             Console.Write("option: ");
             string option = Console.ReadLine();
 
             switch (option)
             {
+                // runs the character checker
                 case "1":
                     checker.CountCharacters(caseSensitive);
                     checker.DisplayCharCounts();
                     break;
 
+                // toggles case sensitivity 
                 case "2":
                     if (caseSensitive == true)
                     {
@@ -48,7 +52,7 @@ public class Program
                     }
 
                     break;
-
+                // exit program
                 case "3":
                     loop = false;
                     break;
